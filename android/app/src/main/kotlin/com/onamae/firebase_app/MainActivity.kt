5a617eb88dc16e0a056f324a5bacdadea23793d6
Y02-Re <ifo@teppen024.co.jp>
2020-10-02 20:04:02 +0900

    First

---
+++ android/app/src/main/kotlin/com/onamae/firebase_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.onamae.firebase_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:firebase_app/main_model.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: ChangeNotifierProvider<MainModel>(
+        create: (_) => MainModel(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Y02塾'),
+          ),
+          body: Consumer<MainModel>(builder: (context, model, child) {
+            return Center(
+              child: Column(
+                children: [
+                  Text(
+                    model.HelloWorld,
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                  ),
+                  RaisedButton(
+                    child: Text('ボタン'),
+                    onPressed: () {
+                      //ここにコード
+                      model.changeHelloWorldText();
+                    },
+                  ),
+                ],
+              ),
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
